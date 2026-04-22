bbfcc94d1b0127f46efba22454da27a4209d647a
sonishweta7021-stack <sonishweta.7021@gmail.com>
2026-04-22 22:58:31 +0530

    Implement student performance risk predictor app

---
+++ aap.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+st.title("🎓 Student Performance Risk Predictor")
+
+df = pd.read_csv("StudentsPerformance.csv")
+
+df["average_score"] = (df["math score"] + df["reading score"] + df["writing score"]) / 3
+
+def risk(score):
+    if score >= 70:
+        return "Low Risk"
+    elif score >= 50:
+        return "Medium Risk"
+    else:
+        return "High Risk"
+
+df["risk"] = df["average_score"].apply(risk)
+
+le = LabelEncoder()
+df["gender"] = le.fit_transform(df["gender"])
+df["lunch"] = le.fit_transform(df["lunch"])
+df["test preparation course"] = le.fit_transform(df["test preparation course"])
+df["risk"] = le.fit_transform(df["risk"])
+
+X = df[["gender","math score","reading score","writing score"]]
+y = df["risk"]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+st.subheader("Enter Student Details")
+
+gender = st.selectbox("Gender", ["Male","Female"])
+math = st.slider("Math Score", 0, 100)
+reading = st.slider("Reading Score", 0, 100)
+writing = st.slider("Writing Score", 0, 100)
+
+if st.button("Predict"):
+    g = 1 if gender=="Male" else 0
+    result = model.predict([[g, math, reading, writing]])
+
+    if result == 0:
+        st.error("High Risk")
+    elif result == 1:
+        st.warning("Medium Risk")
+    else:
+        st.success("Low Risk")
